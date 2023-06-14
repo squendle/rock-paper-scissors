2a31f0ebe78916b83bd64d6df0134319d2c9e5fb
squendle <squendle@gmail.com>
2023-06-13 20:28:07 -0700

    Create getComputerChoice() function to randomly select R/P/S

--- game.js
+++ game.js
@@ -1,17 +1,30 @@
 // My Rock Paper Scissors Game
 //
 //
+// 1. Create function "getComputerChoice()" to randomly return either 'Rock,' 'Paper,' or         'Scissors.'
+//    Return R P or S string randomly.
+//    Return a random value every time the function is called
+//
+// 2. Create a function playRound() that plays a single round of RPS.
+//    Function should take two parameters, playerSelection and computerSelection
+//    Return a string that declares the winner of the round.
+//    playerSelection should be case insensitive, so that the player can enter any caps/lowers
+//    Results of this function should Return, not console.log()
+//   
+// 3. Create a function game() that plays 5 rounds, keeps score, and reports a winner or loser at the end
+//    use console.log to display the results of each round and the winner at the end
 //
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+let choices = [
+  "Rock",
+  "Paper",
+  "Scissors"
+];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random()*choices.length)];
+}
+
