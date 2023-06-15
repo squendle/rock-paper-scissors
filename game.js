de15a983151dba668535035e0ca8b78beebe2196
squendle <squendle@gmail.com>
2023-06-14 17:53:10 -0700

    Create wins and losses counter, display totals after each round

--- game.js
+++ game.js
@@ -1,22 +1,4 @@
 // My Rock Paper Scissors Game
-//
-//
-// 1. Create function "getComputerChoice()" to randomly return either 'Rock,' 'Paper,' or         'Scissors.'
-//    Return R P or S string randomly.
-//    Return a random value every time the function is called
-//
-// 2. Create a function playRound() that plays a single round of RPS.
-//    Function should take two parameters, playerSelection and computerSelection
-//    Return a string that declares the winner of the round.
-//    playerSelection should be case insensitive, so that the player can enter any caps/lowers
-//    Results of this function should Return, not console.log()
-//   
-// 3. Create a function game() that plays 5 rounds, keeps score, and reports a winner or loser at the end
-//    use console.log to display the results of each round and the winner at the end
-//
-//
-//
-//
 
 const choices = [
   "Rock",
@@ -67,12 +49,14 @@ function playRound() {
 
 function game(){
   
-  alert("Let's play Rock, Paper, Scissors!");
+  alert("Let's play Rock, Paper, Scissors! Best of 5 wins...");
+  let wins = 0;
+  let losses = 0;
 
   for (let rounds = 0; rounds < 5; rounds++) {
 
   let roundResult = playRound();
-  
+
     if (roundResult === "It's a Tie! Play again!" || roundResult === 'You must enter "Rock," "Paper," or "Scissors..."') {
       console.log("You both chose the same!");
       console.log(roundResult);
@@ -82,31 +66,43 @@ function game(){
       console.log("You chose Rock");
       console.log("Computer chose Paper");
       console.log(roundResult);
+      losses++;
+      console.log("Wins: " + wins + " " + "Losses: " + losses);
 
     } else if (roundResult === "You win! Rock destroys Scissors!") {
       console.log("You chose Rock");
       console.log("Computer chose Scissors");
       console.log(roundResult);
+      wins++;
+      console.log("Wins: " + wins + " " + "Losses: " + losses);
       
     } else if (roundResult === "You lose! Scissors cut Paper!") {
       console.log("You chose Paper");
       console.log("Computer chose Scissors");
       console.log(roundResult); 
+      losses++;
+      console.log("Wins: " + wins + " " + "Losses: " + losses);
       
     } else if (roundResult === "You win! Paper covers Rock!") {
       console.log("You chose Paper");
       console.log("Computer chose Rock");
       console.log(roundResult); 
+      wins++;
+      console.log("Wins: " + wins + " " + "Losses: " + losses);
       
     } else if (roundResult === "You win! Scissors cut Paper!") {
       console.log("You chose Scissors");
       console.log("Computer chose Paper");
       console.log(roundResult); 
+      wins++;
+      console.log("Wins: " + wins + " " + "Losses: " + losses);
       
     } else if (roundResult === "You lose! Rock destroys Scissors!") {
       console.log("You chose Scissors");
       console.log("Computer chose Rock");
       console.log(roundResult); 
+      losses++;
+      console.log("Wins: " + wins + " " + "Losses: " + losses);
     }
   }
 }
