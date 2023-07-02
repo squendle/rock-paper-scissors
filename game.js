efcec8bb0ac8405367b5c1935345841f2637d634
squendle <squendle@gmail.com>
2023-07-02 14:45:34 -0700

    create scoreboard display and tracking, create play again button

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // Changing colors of choice images, text, and div outlines on mouseover
 
 const choiceDivs = document.querySelectorAll(".choice");
-const choiceText = document.querySelectorAll("div.choiceBox > p");
+const choiceText = document.querySelectorAll("div.choice-box > p");
 const gungnirImg = document.querySelector("#gungnir");
 const mjolnirImg = document.querySelector("#mjolnir");
 const fiskegarnImg = document.querySelector("#fiskegarn");
@@ -38,28 +38,37 @@ function removeChoiceColor(e) {
   }
 };
 
+function changeTextColor(e) {
+  this.classList.toggle('purp-text');
+}
+
 choiceDivs.forEach(choice => choice.addEventListener("mouseover", changeChoiceColor));
 choiceDivs.forEach(choice => choice.addEventListener("mouseout", removeChoiceColor));
 
 
-// My Rock Paper Scissors Game
+// My Mjolnir Fiskegarn Gungnir Game
 
-// array of possible computer choices
-const choices = [
-  "rock",
-  "paper",
-  "scissors"
-];
 
-//the DOM selectors for the divs containing the choices and results
+//the DOM selectors for the divs containing the choices
 const mjolnir = document.querySelector("#rock");
 const fiskegarn = document.querySelector("#paper");
 const gungnir = document.querySelector("#scissors");
 
-const results = document.querySelector("#results");
-const wldDisplay = document.createElement("h2");
+//DOM selectors for top message display
 
-//wld return statements
+const topDisplay = document.querySelector('#top-display');
+const topMessage = document.createElement('h2');
+
+//DOM selectors for results and scoreboard
+
+const resultDisplay = document.querySelector("#results");
+const winCountDisplay = document.querySelector("#win-count");
+const lossCountDisplay = document.querySelector("#loss-count");
+const wldMessage = document.createElement("h2");
+const winCounter = document.createElement("h2");
+const lossCounter = document.createElement("h2");
+
+//wld return statements & counters
 const winRS = "You win! Mjolnir shatters Gungnir!"
 const winPR = "You win! Fiskegarn entangles Mjolnir!"
 const winSP = "You win! Gungnir slices Fiskegarn!"
@@ -67,52 +76,174 @@ const loseRP = "You lose! Fiskegarn entangles Mjolnir!"
 const losePS = "You lose! Gungnir slices Fiskegarn!"
 const loseSR = "You lose! Mjolnir shatters Gungnir!"
 const draw = "It's a draw! Return to battle!"
+let winCount = 0;
+let lossCount = 0;
+
+//play again button stuff
+const playAgain = document.createElement("a");
+playAgain.textContent = "Play Again?";
+playAgain.setAttribute('href', 'javascript:newGame()');
+playAgain.addEventListener("mouseover", changeTextColor);
+playAgain.addEventListener("mouseout", changeTextColor);
+playAgain.addEventListener("click", changeTextColor);
 
 // computerSelection random picker function
 function getComputerChoice() {
   return choices[Math.floor(Math.random()*choices.length)];
 };
 
+let computerSelection;
+
+// array of possible computer choices
+const choices = [
+  "Mjolnir",
+  "Fiskegarn",
+  "Gungnir"
+];
+
 // This is the function for a single round of RPS.
 function playRound(playerSelection) {
-
-  let computerSelection = getComputerChoice();
-
-  if (playerSelection === 'rock' && computerSelection === 'paper'){
-    wldDisplay.textContent = loseRP;
-    results.appendChild(wldDisplay);
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-    wldDisplay.textContent = winRS;
-    results.appendChild(wldDisplay);
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    wldDisplay.textContent = winPR;
-    results.appendChild(wldDisplay);
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    wldDisplay.textContent = losePS;
-    results.appendChild(wldDisplay);
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-    wldDisplay.textContent = loseSP;
-    results.appendChild(wldDisplay);
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    wldDisplay.textContent = winSP;
-    results.appendChild(wldDisplay);
+  if (playerSelection === 'Mjolnir' && computerSelection === 'Fiskegarn'){
+    return loseRP;    
+  } else if (playerSelection === 'Mjolnir' && computerSelection === 'Gungnir'){
+    return winRS;
+  } else if (playerSelection === 'Fiskegarn' && computerSelection === 'Mjolnir') {
+    return winPR;
+  } else if (playerSelection === 'Fiskegarn' && computerSelection === 'Gungnir') {
+    return losePS;
+  } else if (playerSelection === 'Gungnir' && computerSelection === 'Mjolnir'){
+    return loseSR;
+  } else if (playerSelection === 'Gungnir' && computerSelection === 'Fiskegarn') {
+   return winSP;
   } else if (playerSelection === computerSelection) {
-    wldDisplay.textContent = draw;
-    results.appendChild(wldDisplay);
+    return draw;
   } else {
-    wldDisplay.textContent = "";
-    results.appendChild(wldDisplay);
+    return "";
   };
+};
 
+//this updates the score display and various messages
+
+function updateScoreboard(result) {
+  if (result === winRS) {
+    winCount+= 1;
+    wldMessage.textContent = winRS;
+    resultDisplay.appendChild(wldMessage);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+  } else if (result === winPR) {
+    winCount+= 1;
+    wldMessage.textContent = winPR;
+    resultDisplay.appendChild(wldMessage);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+  } else if (result === winSP){
+    winCount+= 1;
+    wldMessage.textContent = winSP;
+    resultDisplay.appendChild(wldMessage);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+  } else if (result === loseRP) {
+    lossCount += 1;
+    wldMessage.textContent = loseRP;
+    resultDisplay.appendChild(wldMessage);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+  } else if (result === losePS) {
+    lossCount += 1;
+    wldMessage.textContent = losePS;
+    resultDisplay.appendChild(wldMessage);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+  } else if (result === loseSR) {
+    lossCount += 1;
+    wldMessage.textContent = loseSR;
+    resultDisplay.appendChild(wldMessage);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+  } else if (result === draw) {
+    wldMessage.textContent = draw;
+    resultDisplay.appendChild(wldMessage);
+    winCounter.textContent = winCount;
+    winCountDisplay.appendChild(winCounter);
+    lossCounter.textContent = lossCount;
+    lossCountDisplay.appendChild(lossCounter);
+  }
+};
+
+// this function checks for a winner after each round and updates the scoreboard and main display accordingly
+
+function checkForWin() {
+  if (winCount === 5){
+    topMessage.textContent = "YOU WIN!";
+    topDisplay.appendChild(topMessage);
+    wldMessage.textContent = "You have pleased the gods! Odin shall welcome you to Valhalla!";
+    resultDisplay.appendChild(wldMessage);
+    winCounter.textContent = "";
+    lossCounter.textContent = "";
+    winCountDisplay.appendChild(winCounter);
+    lossCountDisplay.appendChild(lossCounter);
+    resultDisplay.appendChild(playAgain);
+    resetGame();
+  } else if (lossCount === 5) {
+    topMessage.textContent = "YOU LOSE!"
+    topDisplay.appendChild(topMessage);
+    wldMessage.textContent = "The gods are displeased. Your shame shall follow you forever in Hel.";
+    resultDisplay.appendChild(wldMessage);
+    winCounter.textContent = "";
+    lossCounter.textContent = "";
+    winCountDisplay.appendChild(winCounter);
+    lossCountDisplay.appendChild(lossCounter);
+    resultDisplay.appendChild(playAgain);
+    resetGame();
+  }
+};
+
+//reset game function
+
+function resetGame(){
+  winCount = 0;
+  lossCount = 0;
+};
+
+//new game function
+
+function newGame(){
+  winCount = 0;
+  lossCount = 0;
+  resultDisplay.removeChild(wldMessage);
+  resultDisplay.removeChild(playAgain);
+  topDisplay.removeChild(topMessage);
 };
 
 //DOM event listeners to play rounds.
 mjolnir.addEventListener("click", function() {
-  playRound("rock")
+  computerSelection = getComputerChoice();
+  result = playRound("Mjolnir");
+  updateScoreboard(result);
+  checkForWin();
 });
 fiskegarn.addEventListener("click", function () {
-  playRound("paper")
+  computerSelection = getComputerChoice();
+  result = playRound("Fiskegarn");
+  updateScoreboard(result);
+  checkForWin();
 });
 gungnir.addEventListener("click", function() {
-  playRound("scissors")
+  computerSelection = getComputerChoice();
+  result = playRound("Gungnir");
+  updateScoreboard(result);
+  checkForWin();
 });
